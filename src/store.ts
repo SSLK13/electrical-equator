56ce6e63c8fb26461f557ded12bf5927f14a238f
Cercs <sergiosslk27@gmail.com>
2025-10-14 21:21:42 -0400

    zustand

---
+++ src/store.ts
@@ -0,0 +1,16 @@
+import { type Song } from "./types/song";
+import { create } from "zustand";
+
+export const useSong = create(
+  function (set): {
+    song: ISong | null,
+    updateSong: (song: ISong) => void
+  } {
+    return {
+      song: null,
+      updateSong: function (newSong: ISong) {
+        set({ song: newSong })
+      }
+    }
+  }
+)
